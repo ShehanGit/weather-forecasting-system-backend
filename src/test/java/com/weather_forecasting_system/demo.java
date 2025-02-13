7d082ba259665753da73732dc9538697a82a164e
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2025-02-13 16:51:20 +0530

    test added

---
+++ src/test/java/com/weather_forecasting_system/demo.java
@@ -0,0 +1,12 @@
+package com.weather_forecasting_system;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BasicTest {
+
+    @Test
+    void shouldPass() {
+        assertTrue(true, "This test always passes!");
+    }
+}
