682e1db91f2991e1a03741ee385064d749c570fe
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2025-02-13 16:44:28 +0530

    add in memory database

--- src/test/java/com/weather_forecasting_system/WeatherForecastingSystemApplicationTests.java
+++ src/test/java/com/weather_forecasting_system/WeatherForecastingSystemApplicationTests.java
@@ -2,12 +2,13 @@ package com.weather_forecasting_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeatherForecastingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
