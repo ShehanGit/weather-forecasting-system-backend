01c84e76c90d24c65559fcfb5aeb44e31c6e2d40
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2025-02-23 14:06:37 +0530

    ss

--- src/main/java/com/weather_forecasting_system/WeatherForecastingSystemApplication.java
+++ src/main/java/com/weather_forecasting_system/WeatherForecastingSystemApplication.java
@@ -3,13 +3,11 @@ package com.weather_forecasting_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WeatherForecastingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherForecastingSystemApplication.class, args);
 	}
-
 
 }
